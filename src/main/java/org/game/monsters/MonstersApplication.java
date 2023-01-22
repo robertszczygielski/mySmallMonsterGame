ef6d629b70a6721815dcc5b378843f111d995227
Robert Szczygielski <robert.szczygielski@wp.pl>
2023-01-22 16:11:53 +0100

    project setup

---
+++ src/main/java/org/game/monsters/MonstersApplication.java
@@ -0,0 +1,13 @@
+package org.game.monsters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonstersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonstersApplication.class, args);
+	}
+
+}
